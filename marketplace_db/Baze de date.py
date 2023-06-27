b5590254bc6a882b582f97fe07a2b88130564900
Valentin-Geamanu <valentin.geamanu@yahoo.com>
2023-06-27 20:38:39 +0300

    new files

---
+++ marketplace_db/Baze de date.py
@@ -0,0 +1,21 @@
+
+# SQL data base
+"""
+Vom lucra cu o baza de date SQLite cu ajutorul
+modulului sqlite3 din libraria standard Python.
+
+Astfel vom putea crea o baza de date (intr-un fisier),
+crea tabele si executa query-uri pentru a selecta,
+crea, actualiza si sterge informatii.
+
+Ce vom implementa este o baza de date, cu tabelele corespunzatoare
+astfel incat sa salvam datele dintr-o aplicatie web de tip marketplace.
+
+Intr-o aplicatie de tip marketplace, vom avea:
+- useri
+- produse ce pot fi achizitionate
+- comenzi care pot fi facute de useri,
+astfel ca userii vor putea cumpara intr-o comanda unul sau mai
+multe produse
+"""
+
